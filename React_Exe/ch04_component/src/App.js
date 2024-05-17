eb3768d7f288537fe143a836eca66833392c3097
서혜리 <hye_dam_2@naver.com>
2024-05-17 16:55:07 +0900

    ch04

---
+++ React_Exe/ch04_component/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import Comment from './ex15_extract_component/Comment';
+// import Team from './ex13_compose_component/component/Team';
+// import MyComponent from './ex03_props copy/MyComponent';
+// import Child from '../../ch03_jsx/src/ex01_var copy/Child';
+// import Library from './ex04_props/Library';
+// import MyComponent from './ex05_props/MyComponent';
+// import MyComponent from './ex06_default_props/MyComponent';
+// import MyComponent from './ex09_prop_types/MyComponent';
+// import MyComponent from './ex11_prop_class/MyComponent';
+// import MyComponent from './ex12_prop_class/MyComponent';
+
+function App() {
+  return (
+    // <>
+    //   {/* <Library title='조선 선비들의 '/> */}
+
+    //   <MyComponent>리액트 컴포넌트</MyComponent>
+    // </>
+
+    // <MyComponent name="Happy Happy">리액트 프로그래밍</MyComponent>
+
+    // <MyComponent name="행운">리액트</MyComponent>
+
+    // <MyComponent name="3">리액트</MyComponent>
+
+    // <MyComponent name="React_Programming" favoriteNumber={1}>리액트</MyComponent>
+
+    // <MyComponent name="리액트_뷰_앵귤러" favoriteNumber={1}>리액트</MyComponent>
+
+    // <Team title='리액트 프로젝트' />
+    <Comment/>
+  );
+}
+
+export default App;
---
+++ React_Exe/ch04_component/src/ex06_default_props/MyComponent.js
@@ -0,0 +1,18 @@
+// function MyComponent(props) {
+//     return <div>
+//         안녕하세요, 제 이름은 {props.name}입니다.
+//     </div>
+// }
+
+const MyComponent = (props) => {
+    return (<div>
+        <div>안녕하세요, 제 이름은 {props.name}입니다.</div>
+        <div>안녕하세요, 제 이름은 {props.children}입니다.</div>
+    </div>)
+};
+
+MyComponent.defaultProps = {
+    name: '기본 이름',
+}
+
+export default MyComponent;
