c47166f2bef6a716604bc50e9ac687c78bd58dfc
hyeri-seo <hye_dam_2@naver.com>
2024-05-20 15:28:02 +0900

    스크롤 박스 컴포넌트 렌더링

--- React_Exe/ch07_ref/src/App.js
+++ React_Exe/ch07_ref/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import ValidationSample from './ex02_validation/ValidationSample';
+// import ValidationSample from './ex02_validation/ValidationSample';
+// import ValidationSample from './ex03_validation_ref/ValidationSample';
+// import ValidationSample from './ex04_create_ref/ValidationSample';
+import ScrollBox from './ex05_component_ref/ScrollBox';
 
 function App() {
-  return <ValidationSample/>
+  // return <ValidationSample/>
+  return <ScrollBox/>
 }
 
 export default App;
