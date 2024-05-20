c856e039371adcea7d5308a85def0ade6d3938c7
hyeri-seo <hye_dam_2@naver.com>
2024-05-20 15:33:26 +0900

    App 컴포넌트 내부에 ref 달고 내부 메서드 사용

--- React_Exe/ch07_ref/src/App.js
+++ React_Exe/ch07_ref/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 // import ValidationSample from './ex02_validation/ValidationSample';
 // import ValidationSample from './ex03_validation_ref/ValidationSample';
 // import ValidationSample from './ex04_create_ref/ValidationSample';
 import ScrollBox from './ex05_component_ref/ScrollBox';
 
-function App() {
+class App extends Component {
   // return <ValidationSample/>
-  return <ScrollBox/>
+  // return <ScrollBox/>
+  render() {
+    return (
+      <div>
+          <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+          <button onClick={() => this.ScrollBox.scrollToBottom()}>
+            맨 밑으로
+          </button>
+      </div>
+      );
+  }
 }
 
 export default App;
