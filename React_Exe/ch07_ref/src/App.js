6ca9498b2b8ed523427c5e2183e8293abd97cbec
hyeri-seo <hye_dam_2@naver.com>
2024-05-20 14:48:09 +0900

    ch07_ref

---
+++ React_Exe/ch07_ref/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import ValidationSample from './ex02_validation/ValidationSample';
+
+function App() {
+  return <ValidationSample/>
+}
+
+export default App;
