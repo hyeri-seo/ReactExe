dea34dafd77efdfc6550acc57d4903ae1156036a
hyeri-seo <hye_dam_2@naver.com>
2024-05-20 11:50:09 +0900

    App.js

--- React_Exe/ch05_state/src/App.js
+++ React_Exe/ch05_state/src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 // import Counter from './ex01_class_state/Counter';
-import Counter from './ex02_class_multi_state/Counter';
+// import Counter from './ex02_class_multi_state/Counter';
+// import Counter from './ex03_class_conti_call/Counter';
+// import Counter from './ex04_class_conti_call/Counter';
+// import Counter from './ex05_class_later_state/Counter';
+// import Say from './ex06_function_state/Say';
+import Say from './ex07_function_multi_state/Say';
 
 function App() {
-  return <Counter/>;
+  // return <Counter/>;
+  return <Say/>
 }
 
 export default App;
