a424d3ede0af5dd90ffd7081e745907e231d6e8e
hyeri-seo <hye_dam_2@naver.com>
2024-05-20 10:07:24 +0900

    class state

---
+++ React_Exe/ch05_state/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+// import Counter from './ex01_class_state/Counter';
+import Counter from './ex02_class_multi_state/Counter';
+
+function App() {
+  return <Counter/>;
+}
+
+export default App;
