e85ce0aaed5b82c74679ae5408d7e5f3e284b55c
hyeri-seo <hye_dam_2@naver.com>
2024-05-21 10:59:14 +0900

    useEffect

--- React_Exe/ch10_hooks/src/ex03_useEffect/info.jsx
+++ React_Exe/ch10_hooks/src/ex03_useEffect/info.jsx
@@ -1,5 +1,26 @@
 import React, { useState, useEffect } from "react";
 
+/*
+  클래스 컴포넌트: Life Cycle
+  함수 컴포넌트: Life Cycle -> useEffect
+
+  [클래스]
+  componentDidMount(마운트)
+  componentDidUpdate(업데이트)
+  componentWillUnmount(언마운트)
+
+  [함수]
+  useEffect(()=>{});  업데이트시마다 등록된 함수가 매번 호출됨
+  useEffect(()=>{}, []);  마운트 시 한 번만 호출됨
+  useEffect(()=>{}, [name]);  name state가 변화되었을 때 호출됨
+  useEffect(()=>{
+    name state가 변화되었을 때 호출되는 부분
+    return() => {
+      업데이트 직전/언마운트 되기 전
+    }
+  }, [name]);
+*/
+
 const Info = () => {
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
