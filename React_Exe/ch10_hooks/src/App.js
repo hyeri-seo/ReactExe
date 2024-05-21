fa788575e445e20a5b85417ba67ad4731f34ee68
hyeri-seo <hye_dam_2@naver.com>
2024-05-21 10:46:10 +0900

    ch10_hooks

---
+++ React_Exe/ch10_hooks/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+// import Counter from './ex01_useState/Counter';
+// import Info from './ex02_useState/info';
+import Info from './ex03_useEffect/info';
+
+function App() {
+  // return <Counter/>
+  return <Info/>
+}
+
+export default App;
