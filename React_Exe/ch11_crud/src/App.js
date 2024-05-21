a4f23c6239bd4b5f5eb6fb7d633aef81ce13382e
hyeri-seo <hye_dam_2@naver.com>
2024-05-21 17:18:56 +0900

    ch11_crud App.js

--- React_Exe/ch11_crud/src/App.js
+++ React_Exe/ch11_crud/src/App.js
@@ -7,7 +7,12 @@ import './App.css';
 // import SubApp from './ex06_nav_list/SubApp';
 // import SubApp from './ex07_nav_list/SubApp';
 // import SubApp from './ex08_nav_list/SubApp';
-import SubApp from './ex09_list_key/SubApp';
+// import SubApp from './ex09_list_key/SubApp';
+// import SubApp from './ex10_event/SubApp';
+// import SubApp from './ex11_arrow/SubApp';
+// import SubApp from './ex12_nav_event/SubApp';
+// import SubApp from './ex13_state/SubApp';
+import SubApp from './ex14_state/SubApp';
 
 function App() {
   return <SubApp/>;
