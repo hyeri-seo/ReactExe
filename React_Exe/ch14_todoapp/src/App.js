5f4e6e3108a36baeab476b256924a02f8768db79
hyeri-seo <hye_dam_2@naver.com>
2024-05-25 23:33:04 +0900

    ch14

---
+++ React_Exe/ch14_todoapp/src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+import { useState, useRef, useCallback } from "react";
+import TodoTemplate from "./ex05_TodoInsert/TodoTemplate";
+import TodoInsert from "./ex05_TodoInsert/TodoInsert";
+import TodoList from "./ex05_TodoInsert/TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링 해보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "일정 관리 앱 만들어 보기",
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
+    </TodoTemplate>
+  );
+}
+
+export default App;
