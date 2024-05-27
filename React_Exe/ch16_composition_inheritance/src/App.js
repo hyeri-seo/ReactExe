ef5d141927632136247880f63f08d4d000656520
hyeri-seo <hye_dam_2@naver.com>
2024-05-27 13:29:46 +0900

    ch16_상속

--- React_Exe/ch16_composition_inheritance/src/App.js
+++ React_Exe/ch16_composition_inheritance/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+// import WelcomeDialog from './ex02_containment/WelcomDialog';
+// import SplitPane from './ex02_containment/SplitPane';
+// import WelcomeDesc from './ex02_containment/WelcomeDesc';
+// import AlarmDialog from './ex03_specialization/AlarmDialog';
+import ProfileCard from './ex04_inheritance/ProfileCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+    // <div style={{flexDirection: "row"}} className='App'>
+    //   <WelcomeDialog/>
+    //   <hr/>
+    //   <WelcomeDialog/>
+    //   <hr/>
+    //   {/* children이 여러 개일 경우 */}
+    //   <SplitPane
+    //     left={<WelcomeDesc/>}
+    //     right={<WelcomeDialog/>}
+    //   />
+    //   <hr/>
+    //   <AlarmDialog/>
+    // </div>
+  // )
+  return <ProfileCard/>
 }
 
 export default App;
