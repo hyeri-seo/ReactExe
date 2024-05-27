ff49adcf573f94f6e723fb20a1173b96a9c731c4
hyeri-seo <hye_dam_2@naver.com>
2024-05-27 13:40:32 +0900

    ch16

--- React_Exe/ch16_composition_inheritance/src/App.js
+++ React_Exe/ch16_composition_inheritance/src/App.js
@@ -1,28 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-// import WelcomeDialog from './ex02_containment/WelcomDialog';
-// import SplitPane from './ex02_containment/SplitPane';
-// import WelcomeDesc from './ex02_containment/WelcomeDesc';
-// import AlarmDialog from './ex03_specialization/AlarmDialog';
-import ProfileCard from './ex04_inheritance/ProfileCard';
+import logo from "./logo.svg";
+import "./App.css";
+import WelcomeDialog from "./ex01_containment/WelcomeDialog";
+import WelcomeDesc from "./ex01_containment/WelcomeDesc";
+import SplitPane from "./ex02_containment/SplitPane";
+import AlarmDialog from "./ex03_specialization/AlarmDialog";
+import ProfileCard from "./ex04_inheritance/ProfileCard";
 
 function App() {
-  // return (
-    // <div style={{flexDirection: "row"}} className='App'>
-    //   <WelcomeDialog/>
-    //   <hr/>
-    //   <WelcomeDialog/>
-    //   <hr/>
-    //   {/* children이 여러 개일 경우 */}
-    //   <SplitPane
-    //     left={<WelcomeDesc/>}
-    //     right={<WelcomeDialog/>}
-    //   />
-    //   <hr/>
-    //   <AlarmDialog/>
+  return (
+    // <div className="App">
+    //   <WelcomeDialog />
+    //   <hr />
+    //   <WelcomeDesc />
+    //   <hr />
+    //   {/* children이 여러개일 경우 props의 속성이름을 여러개 지정해준다 */}
+    //   <SplitPane left={<WelcomeDesc />} right={<WelcomeDialog />} />
+    //   <hr />
+    //   <AlarmDialog />
     // </div>
-  // )
-  return <ProfileCard/>
+    <ProfileCard />
+  );
 }
 
 export default App;
