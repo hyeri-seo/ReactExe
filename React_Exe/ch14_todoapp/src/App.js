1de8d0ec542a76b04524ffbe664cd947125e56d2
hyeri-seo <hye_dam_2@naver.com>
2024-05-27 10:29:25 +0900

    ch14

--- React_Exe/ch14_todoapp/src/App.js
+++ React_Exe/ch14_todoapp/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { useState, useRef, useCallback } from "react";
-import TodoTemplate from "./ex05_TodoInsert/TodoTemplate";
-import TodoInsert from "./ex05_TodoInsert/TodoInsert";
-import TodoList from "./ex05_TodoInsert/TodoList";
+import TodoTemplate from "./ex07_toggle/TodoTemplate";
+import TodoInsert from "./ex07_toggle/TodoInsert";
+import TodoList from "./ex07_toggle/TodoList";
 
 function App() {
   const [todos, setTodos] = useState([
@@ -38,10 +38,29 @@ function App() {
     [todos]
   );
 
+  const onRemove = useCallback(
+    (id) => {
+      // 기존에 있는 id에 해당하지 않는 항목만 저장
+      setTodos(todos.filter(todo => todo.id !== id));
+    },
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo => 
+          todo.id === id ? {...todo, checked: !todo.checked} : todo,
+        )
+      )
+    },
+    [todos]
+  )
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
