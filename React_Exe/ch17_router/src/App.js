42f2aafecc0a65ba3168e19f1d73e613a8377c9c
hyeri-seo <hye_dam_2@naver.com>
2024-05-29 10:03:20 +0900

    route 사용하기

--- React_Exe/ch17_router/src/App.js
+++ React_Exe/ch17_router/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import SubApp from './ex02_sub_app/SubApp';
+// import SubApp from './ex02_sub_app copy/SubApp';
+import SubApp from './ex03_routes/SubApp';
 
 function App() {
   return <SubApp/>
