4bd62ebeaea91d0ee2480454eeee985a0dee5242
hyeri-seo <hye_dam_2@naver.com>
2024-05-30 11:12:34 +0900

    comment_list

--- React_Exe/ch18_mini_blog/src/App.js
+++ React_Exe/ch18_mini_blog/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import MiniBlogApp from './ex03_mini_blog_app/MiniBlogApp';
+import MiniBlogApp from './ex06_post_view_page/MiniBlogApp';
 
 function App() {
   return <MiniBlogApp/>
