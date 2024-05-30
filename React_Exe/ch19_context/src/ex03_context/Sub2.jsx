a4e48f02bea6e76764d005295bdb90e648b4b3dc
hyeri-seo <hye_dam_2@naver.com>
2024-05-30 12:15:10 +0900

    context

---
+++ React_Exe/ch19_context/src/ex03_context/Sub2.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+import Sub3 from './Sub3';
+
+function Sub2() {
+    return (
+        <div>
+            <h1>Sub2</h1>
+            <Sub3/>
+        </div>
+    )
+}
+
+export default Sub2;
---
+++ React_Exe/ch19_context/src/ex03_context/Sub3.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Sub3() {
+    return (
+        <div>
+            <h1>Sub3</h1>
+        </div>
+    )
+}
+
+export default Sub3;
