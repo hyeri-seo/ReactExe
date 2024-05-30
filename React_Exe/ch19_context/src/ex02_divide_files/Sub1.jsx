540a93a36a3131c99e0f5bfe9faf5215c9c3b0c8
hyeri-seo <hye_dam_2@naver.com>
2024-05-30 11:43:14 +0900

    파일 분리해도 계층 구조 유지

---
+++ React_Exe/ch19_context/src/ex02_divide_files/Sub1.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+import Sub2 from './Sub2';
+
+function Sub1() {
+    return (
+        <div>
+            <h1>Sub1</h1>
+            <Sub2/>
+        </div>
+    )
+}
+
+export default Sub1;
