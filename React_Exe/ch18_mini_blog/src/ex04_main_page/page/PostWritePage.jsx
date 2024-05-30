b0d39b7a5bac9653c5aaebcfbf3bc8c5bdd73cf1
hyeri-seo <hye_dam_2@naver.com>
2024-05-30 10:42:37 +0900

    main_page

---
+++ React_Exe/ch18_mini_blog/src/ex04_main_page/page/PostWritePage.jsx
@@ -0,0 +1,23 @@
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    :not(:last-child) {
+        margin-bottom: 16px;
+    }
+`;
+
+function PostWritePage(props) {
+    return (
+        
+    )
+}
