a686f6d24ebeccff54c6a0f9d8a92acd5ad91f4b
hyeri-seo <hye_dam_2@naver.com>
2024-06-03 15:45:03 +0900

    SecurityConfig에서 configure 설정에서 프론트 개발의 원활한 진행으 ㄹ위해서 모든 요청 허용하는 것으로 변경

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import MainApp from './ex01_main_app/MainApp';
+import MainApp from './ex02_car_list/MainApp';
 
 function App() {
   return <MainApp/>
